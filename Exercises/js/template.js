b737ab779949265b009c747ec6982d56f0555395
muneeb202 <muneebarshad2002@gmail.com>
2023-12-12 15:03:50 +0500

    Exercise 1

--- Exercises/js/template.js
+++ Exercises/js/template.js
@@ -1,5 +1,33 @@
 module.exports = {
 
+    // Exercise 1 - Iris Species Classifier
+    exercise1: (SepalLen, SepalWid, PetalLen, PetalWid) => {
+        if(PetalLen < 2.5){
+            return ("setosa")
+        }
+        else{
+            if(PetalWid < 1.8){
+                if(PetalLen < 5 && PetalWid < 1.7){
+                    return("versicolor")
+                }
+                else if( PetalLen<5 && !PetalWid<1.7){
+                    return("virginica")
+                }
+                else if(!PetalLen < 5 && PetalWid >= 1.6 && SepalLen < 7 ){return ("versicolor")}
+                else if(!PetalLen < 5 && PetalWid >= 1.6 && !SepalLen < 7 ){return("verginica")}
+                else if(!PetalLen < 5 && !PetalWid >= 1.6 ){return("verginica")}
+                
+            }
+            else{
+                if(PetalLen<4.9 && SepalLen<6){
+                    return("versicolor")
+                }
+                else if(PetalLen<4.9 && !SepalLen<6){return("verginica")}
+                else if(!PetalLen<4.9){return("verginica")}
+            }
+        }
+    },
+
 }
 
 
